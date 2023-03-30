aa176b56d40dca710ae7ac330a31749e83edf5ef
Travis <travisdbirch@gmail.com>
2023-03-30 16:07:01 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='chicagohouses',
       version='0.0.1',
       description='Get a filterable list of houses in Chicago.',
       author='Travis Birch',
       author_email='aml-toolbox-feedback@googlegroups.com',
-      packages=['chicagohouses', 'data'],
-      install_requires=['polars', 'geopandas']
+      packages=find_packages(),
+      requires=['polars', 'geopandas']
      )
